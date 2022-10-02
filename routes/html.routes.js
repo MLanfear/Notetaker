b74aeef27b308ccfda799023394e0fda04f501fe
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-10-02 16:52:27 -0700

    Update html.routes.js

--- routes/html.routes.js
+++ routes/html.routes.js
@@ -4,11 +4,13 @@ const path = require("path");
 //Routes
 
 module.exports = (app) => {
-    app.get("/notes", (req, res) => {
-        res.sendFile(path.join(__dirname, "../notes.html"));
+
+    app.get("/index", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../index.html"));
+
+    app.get("/notes", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 };
